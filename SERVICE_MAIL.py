29314935228ddfa41abeafb7a135dca9422217f5
kitrakatpug21 <50409869+kitrakatpug21@users.noreply.github.com>
2021-07-23 17:04:03 +0530

    Update SERVICE_MAIL.py

--- SERVICE_MAIL.py
+++ SERVICE_MAIL.py
@@ -2,12 +2,11 @@
 import smtplib
 import pyttsx3
 ################## initialize #####################
-sender = 'voicerowave.major@gmail.com'
-receiver1 = 'kitrakatpug21@gmail.com'
-receiver2 = 'manasvibhargava18@gmail.com'
+sender = 'voi*********or@srmist.edu.in'
+receiver1 = "HIDDEN"
 message = "THIS IS TO INFORM THAT THE SERVICE BUTTON HAD BEEN PRESSED IN THE VOICEROWAVE OVEN. KINDLY CONTACT THE CUSTOMER"
-username = 'voicerowave.major@gmail.com'  
-password = 'VOICEROWAVE123'
+username = 'voi*********or@srmist.edu.in'  
+password = "Hidden"
 engine = pyttsx3.init()       ##Text to Speech Initialized
 engine.setProperty('rate', 160)
 voices = engine.getProperty('voices')
@@ -18,7 +17,6 @@ try:
     server.starttls()
     server.login(username, password)               ##login using provided credentials
     server.sendmail(sender, receiver1, message)    ##sending mail receiver1
-    server.sendmail(sender, receiver2, message)    ##sending mail receiver2
     server.quit()
     engine.setProperty('voice', voices.id)
     engine.say("AN EMAIL IS SENT SUCCESSSFULLY REGARDING THE SERVICE REQUEST")
